69c2cc7d78433b0e01e10b853d4ae159d36aca3d
Gabriel José Bazán <gbazan@outlook.com>
2018-03-18 19:55:48 -0300

    Server host configuration

--- src/app/settings.ts
+++ src/app/settings.ts
@@ -1,7 +1,8 @@
 
 export class Settings {
-  static CONFIGURATION_ENDPOINT = 'http://192.168.1.103/configuration/';
-  static WPS_ENDPOINT = 'http://192.168.1.103/wps/';
-  static EXECUTIONS_ENDPOINT = 'http://192.168.1.103/executions/';
-  static REPOSITORY_ENDPOINT = 'http://192.168.1.103/repository/geoserver/';
+  static HOST = 'http://192.168.1.103';
+  static CONFIGURATION_ENDPOINT = `${Settings.HOST}/configuration/`;
+  static WPS_ENDPOINT = `${Settings.HOST}/wps/`;
+  static EXECUTIONS_ENDPOINT = `${Settings.HOST}/executions/`;
+  static REPOSITORY_ENDPOINT = `${Settings.HOST}/repository/geoserver/`;
 }
